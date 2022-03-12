77e6bc0d0367c7b8c007267291fc6bde79eed24e
AndreiTrif88 <101469868+AndreiTrif88@users.noreply.github.com>
2022-03-12 18:27:18 +0200

    Create ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+  ifstream f("bac.txt");
+    int  n,a[100],j,p,x,i,aux,ok=0;
+    n=0;
+    f>>x;
+    n++;
+    a[n]=x;
+    p=x;
+    while(f>>x)
+    {
+        n++;
+        a[n]=x;
+        
+    }
+        for(i=1;i<=n-1;i++)
+        for(j=i+1;j<=n;j++)
+            if (a[i]>a[j])
+            {
+                aux=a[i];
+                a[i]=a[j];
+                a[j]=aux;
+            }
+        for(i=1;i<=n;i++)
+            if(a[i]==p && ok==0)
+            {
+                cout <<i;
+                ok=1;
+            }
+    
+}
